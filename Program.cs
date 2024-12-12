4af28a02bd6edf43f480d3061d7840a18b59cd24
aysesogut03 <aysesogut2003@gmail.com>
2024-12-12 23:20:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            calıisan.isim = "Ayşe";
+            Console.WriteLine("isim:" + calıisan.isim);
+            Console.ReadLine();
+        }
+    }
+}
